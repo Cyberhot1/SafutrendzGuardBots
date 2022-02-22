dfd57ccb2d01f47621c87409d42bcd29fae8d89d
Cyberhot1 <66538020+Cyberhot1@users.noreply.github.com>
2022-02-22 21:46:34 +0100

    Update responses.py

--- responses.py
+++ responses.py
@@ -1,23 +1,29 @@
 from datetime import datetime
 
-
-
 def sample_responses(input_text):
     user_message = str(input_text).lower()
-        
 
     if user_message in ("when moon", "moon when", "when lambo"):
         return "start shilling to moon your own bag, type /shill to get shill groups"
+    
+    if user_message in ("when launch"):
+        return "Launch Date TBA, \n wait for announcement in the Group"
+    
+    if user_message in ("who can i contact for marketing", "marketing proposal", "marketing", "Marketing"):
+        return "For marketing proposal, \n Kindly Mail us on marketing@safutrendz.com "
+    
+    if user_message in ("who can i contact for partnership", "partnerhip proposal", "partnership", "Partnership"):
+        return "For partnership proposal, \n Kindly Mail us on partnership@safutrendz.com "
 
     if user_message in ("who are you", "who are you?"):
         return "I'm the SaFuTrendz Guard Bot"
 
-    if user_message in ("where is admin", "where is admin?"):
+    if user_message in ("where is admin", "where is admin?", "where is admin", "where is the admin"):
         return "You can find the admin by typing /admin"
 
     if user_message in ("time", "what is the time?"):
         now = datetime.now()
-        date_time = now.strftime("⏰ %m/%d/%Y, %H:%M:%S")
+        date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
 
         return str(date_time)
 
